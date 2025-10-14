7c42dd16f6eb1f3368b09d969ce48988133cb1b3
guilhermesegovia <guilhermemachadosegovia@gmail.com>
2025-10-14 20:55:06 -0300

    first commit

---
+++ src/model/Empresa.ts
@@ -0,0 +1,9 @@
+export class Empresa {
+    constructor(
+        private empresa: string,
+        private endereco: string,
+        private cpnj: string,
+        private email: string,
+        private senha: string
+    )  {}
+}
---
+++ src/model/Epi.ts
@@ -0,0 +1,11 @@
+export class Epi{
+    constructor(
+        private nome: string,
+        private ca: number,
+        private modo_uso: string,
+        private validade: string,
+        private tipo: string,
+        private fabricante: string,
+        private data_entrada: string,
+    ){}
+}
